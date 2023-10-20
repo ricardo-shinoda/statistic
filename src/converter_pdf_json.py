a19ff027d69020665b553edd7b07e1193eca5f8c
ricardo-shinoda <ricardoshinoda@gmail.com>
2023-10-19 21:21:02 -0300

    fix: json output

--- src/converter_pdf_json.py
+++ src/converter_pdf_json.py
@@ -1,7 +1,8 @@
+import re
+from datetime import datetime
 from decouple import config
 import PyPDF2
 import json
-import re
 
 pdf_password = config('PDF_PASSWORD')
 
@@ -23,18 +24,26 @@ for page_num in range(len(pdf_reader.pages)):
     for line in lines:
         if re.search(date_pattern, line):
             if movement:
-                movements.append(movement)
+                if movement.get("Date") and movement.get("Transaction"):
+                    movements.append(movement)
             parts = line.strip().split()
             current_date = parts[0]
             description = " ".join(parts[1:-2])
             montante = parts[-2]
             natureza = parts[-1]
-            movement = {"Date": current_date, "Transaction": description, "Montante": montante, "Natureza": natureza, "Transactions": []}
+            if re.match(date_pattern, current_date):
+                if "SALDO" in description:
+                    movement = None  # Exclude transactions with "SALDO" in description
+                elif not natureza.isalpha():
+                    movement = None  # Exclude transactions where "Natureza" is not a letter
+                else:
+                    movement = {"Date": current_date, "Transaction": description, "Montante": montante, "Natureza": natureza, "Transactions": []}
         else:
             if movement is not None:
-                movement["Transactions"].append(line.strip())
+                if "SALDO" not in line:
+                    movement["Transactions"].append({"Transaction": line.strip()})
 
-if movement:
+if movement and movement.get("Date") and movement.get("Transaction"):
     movements.append(movement)
 
 pdf_file.close()
@@ -51,9 +60,15 @@ movements_data = {
 
 data.update(movements_data)
 
+# Filter out transactions with "SALDO" in their description and non-letter "Natureza"
+filtered_movements = [m for m in movements if all("SALDO" not in t.get("Transaction", "") for t in m.get("Transactions", []))]
+
+data["movements"] = filtered_movements
+
 json_data = json.dumps(data, ensure_ascii=False, indent=4)
 
 with open('output_account.json', 'w') as json_file:
     json_file.write(json_data)
 
 print(json_data)
+
