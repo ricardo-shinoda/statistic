c8d824983ae08e6ee4c6e27d2492a66dc0da6bdf
ricardo-shinoda <ricardoshinoda@gmail.com>
2023-11-06 21:29:04 -0300

    feat: [wip] update a tab with some info from another sheet

--- src/drive_upload_tab.py
+++ src/drive_upload_tab.py
@@ -1,17 +1,52 @@
 import openpyxl
+import os
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
 from googleapiclient.http import MediaFileUpload
 
-file_name = "2023-08"
+# Especificação dos arquivos de origem e destino
+source_file_path = '/home/ricardo/code/statistic/src/credit_card/xlsx/2023-09.xlsx'
+target_file_path = '/home/ricardo/code/statistic/src/Controle.xlsx'
+
+# Nome da aba de origem e nome da nova aba de destino
+source_sheet_name = 'Sheet1'
+new_target_sheet_name = 'new'
+
+# Carrega a aba de origem usando openpyxl
+source_workbook = openpyxl.load_workbook(source_file_path, data_only=True)
+source_sheet = source_workbook[source_sheet_name]
+
+# Carrega a planilha de destino (ou cria uma nova se não existir)
+if os.path.exists(target_file_path):
+    target_workbook = openpyxl.load_workbook(target_file_path)
+    target_sheet = target_workbook[new_target_sheet_name]
+else:
+    target_workbook = openpyxl.Workbook()
+    target_sheet = target_workbook.active
+    target_sheet.title = new_target_sheet_name
+
+# Copia os dados da aba de origem para a aba de destino, ignorando linhas em branco
+for row in source_sheet.iter_rows(values_only=True):
+    if any(row):
+        target_sheet.append(row)
+
+# Salva a planilha de destino
+target_workbook.save(target_file_path)
+
+# Faz o upload da planilha para o Google Drive
+
+file_name = "2023-08"  # Nome do arquivo no Google Drive
 
 SCOPES = ['https://www.googleapis.com/auth/drive']
 SERVICE_ACCOUNT_FILE = 'service.account.json'
 PARENT_FOLDER_ID = "1qyMZb6P7H5oaq2zxoaqs17VJWSnHNgnG"
 
+
 def authenticate():
-    creds = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+    creds = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
     return creds
+
 
 def upload_file(file_path):
     creds = authenticate()
@@ -22,45 +57,12 @@ def upload_file(file_path):
         'parents': [PARENT_FOLDER_ID]
     }
 
-    try:
-        # Upload the file to Google Drive
-        media = MediaFileUpload(file_path, resumable=True)
-        file = service.files().create(
-            body=file_metadata,
-            media_body=media,
-            fields='id'
-        ).execute()
+    media_body = MediaFileUpload(file_path, resumable=True)
 
-        # Get the file ID of the uploaded file
-        uploaded_file_id = file.get('id')
+    file = service.files().create(
+        body=file_metadata,
+        media_body=media_body
+    ).execute()
 
-        # Specify the source file and sheet names
-        source_file = '/home/ricardo/code/statistic/src/credit_card/xlsx/2023-09.xlsx'  # Source file path
-        source_sheet_name = 'Sheet1'  # Source sheet name
 
-        # Open the source workbook
-        source_workbook = openpyxl.load_workbook(source_file, data_only=True)
-
-        # Get the source worksheet
-        source_sheet = source_workbook[source_sheet_name]
-
-        # Specify the target file and sheet names
-        target_file_path = '/home/ricardo/code/statistic/src/Controle.xlsx'  # Target file path
-        target_sheet_name = 'new'  # Target sheet name
-
-        # Open the target workbook
-        target_workbook = openpyxl.load_workbook(target_file_path, data_only=True)
-
-        # Get the target worksheet
-        target_sheet = target_workbook[target_sheet_name]
-
-        # Copy the contents of the source worksheet to the target worksheet
-        for row in source_sheet.iter_rows(values_only=True):
-            target_sheet.append(row)
-
-        # Save the changes to the target workbook
-        target_workbook.save(target_file_path)
-    except Exception as e:
-        print(f"An error occurred: {str(e)}")
-
-upload_file(f'/home/ricardo/code/statistic/src/credit_card/xlsx/{file_name}.xlsx')
+upload_file(target_file_path)
