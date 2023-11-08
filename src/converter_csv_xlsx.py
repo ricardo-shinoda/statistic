e61384b97b950ba8c764f592ea6c14417cc09bbe
ricardo-shinoda <ricardoshinoda@gmail.com>
2023-11-07 22:10:24 -0300

    feat: Add file to the Controle tab, as a new tab 99% done

--- src/converter_csv_xlsx.py
+++ src/converter_csv_xlsx.py
@@ -1,19 +1,37 @@
+# import pandas as pd
+
+
+# # rename this variable to save the file according to the invoice month
+# month = "2023-09"
+
+# # Read the CSV file with the specified delimiter
+# df = pd.read_csv('invoice.csv', delimiter=';')
+
+# # Format the numbers in the DataFrame to use Brazilian standards
+# df['Valor (em R$)'] = df['Valor (em R$)'].apply(lambda x: f'{x:,.2f}'.replace('.', 'X').replace(',', '.').replace('X', ','))
+
+# # Exclude rows with "Inclusao de Pagamento" in the "Descrição" column
+# df = df[df['Descrição'] != 'Inclusao de Pagamento    ']
+
+# # Save the DataFrame to an Excel file with the desired format
+# df.to_excel(f'/home/ricardo/code/statistic/src/credit_card/xlsx/{month}.xlsx', index=False, float_format="%.2f")
+
+# print("Conversion complete, Data saved in 'output.xlsx'.")
+
+
 import pandas as pd
 
-
 # rename this variable to save the file according to the invoice month
-month = "2023-09"
+month = "2023-99"
 
 # Read the CSV file with the specified delimiter
 df = pd.read_csv('invoice.csv', delimiter=';')
-
-# Format the numbers in the DataFrame to use Brazilian standards
-df['Valor (em R$)'] = df['Valor (em R$)'].apply(lambda x: f'{x:,.2f}'.replace('.', 'X').replace(',', '.').replace('X', ','))
 
 # Exclude rows with "Inclusao de Pagamento" in the "Descrição" column
 df = df[df['Descrição'] != 'Inclusao de Pagamento    ']
 
 # Save the DataFrame to an Excel file with the desired format
-df.to_excel(f'/home/ricardo/code/statistic/src/credit_card/xlsx/{month}.xlsx', index=False, float_format="%.2f")
+df.to_excel(f'/home/ricardo/code/statistic/src/credit_card/xlsx/{
+            month}.xlsx', index=False, float_format="%.2f")
 
 print("Conversion complete, Data saved in 'output.xlsx'.")
--- src/drive_upload_tab.py
+++ src/drive_upload_tab.py
@@ -4,13 +4,16 @@ from googleapiclient.discovery import build
 from google.oauth2 import service_account
 from googleapiclient.http import MediaFileUpload
 
+file_name = '2023-99'
+
 # Especificação dos arquivos de origem e destino
-source_file_path = '/home/ricardo/code/statistic/src/credit_card/xlsx/2023-09.xlsx'
+source_file_path = f'/home/ricardo/code/statistic/src/credit_card/xlsx/{
+    file_name}.xlsx'
 target_file_path = '/home/ricardo/code/statistic/src/Controle.xlsx'
 
 # Nome da aba de origem e nome da nova aba de destino
 source_sheet_name = 'Sheet1'
-new_target_sheet_name = 'new'
+new_target_sheet_name = f'{file_name}'  # Change the sheet name to 'Controle2'
 
 # Carrega a aba de origem usando openpyxl
 source_workbook = openpyxl.load_workbook(source_file_path, data_only=True)
@@ -19,23 +22,23 @@ source_sheet = source_workbook[source_sheet_name]
 # Carrega a planilha de destino (ou cria uma nova se não existir)
 if os.path.exists(target_file_path):
     target_workbook = openpyxl.load_workbook(target_file_path)
-    target_sheet = target_workbook[new_target_sheet_name]
 else:
     target_workbook = openpyxl.Workbook()
-    target_sheet = target_workbook.active
-    target_sheet.title = new_target_sheet_name
 
-# Copia os dados da aba de origem para a aba de destino, ignorando linhas em branco
+# Create a new sheet named 'Controle2' or get it if it already exists
+if new_target_sheet_name in target_workbook.sheetnames:
+    target_sheet = target_workbook[new_target_sheet_name]
+else:
+    target_sheet = target_workbook.create_sheet(title=new_target_sheet_name)
+
+# Copia os dados da aba de origem para a aba de destino
 for row in source_sheet.iter_rows(values_only=True):
-    if any(row):
-        target_sheet.append(row)
+    target_sheet.append(row)
 
 # Salva a planilha de destino
 target_workbook.save(target_file_path)
 
 # Faz o upload da planilha para o Google Drive
-
-file_name = "2023-08"  # Nome do arquivo no Google Drive
 
 SCOPES = ['https://www.googleapis.com/auth/drive']
 SERVICE_ACCOUNT_FILE = 'service.account.json'
@@ -53,7 +56,7 @@ def upload_file(file_path):
     service = build('drive', 'v3', credentials=creds)
 
     file_metadata = {
-        'name': f'{file_name}.xlsx',
+        'name': 'Controle.xlsx',
         'parents': [PARENT_FOLDER_ID]
     }
 
@@ -65,4 +68,5 @@ def upload_file(file_path):
     ).execute()
 
 
+# Upload the target file to Google Drive
 upload_file(target_file_path)
