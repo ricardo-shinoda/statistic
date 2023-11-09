23eb28ee73e1547db29ee3a7f253433ac8c4526a
ricardo-shinoda <ricardoshinoda@gmail.com>
2023-11-09 20:25:11 -0300

    search csv file at download folder and convert to xlsx and json

---
+++ src/control.py
@@ -0,0 +1,3 @@
+# This is going to be the main control scrip
+# Need to
+   - move the fil#ad Controle and move to repo
--- src/search_converter_cc.py
+++ src/search_converter_cc.py
@@ -1,5 +1,3 @@
-# This is going to be the file to search for an invoice on Downloads page on My computer
-# and move it to this repository
 import os
 import shutil
 import pandas as pd
@@ -8,11 +6,13 @@ import json
 source_directory = "/home/ricardo/Downloads/invoice"
 
 # Rename it as needed
-month = "2023-00"
+month = "2022-10"
 
 target_extension = ".csv"
 
 destination_directory = "/home/ricardo/code/statistic/src"
+
+destination_file_path = None  # Initialize the variable outside the loop
 
 for root, dirs, files in os.walk(source_directory):
     for file in files:
@@ -22,20 +22,32 @@ for root, dirs, files in os.walk(source_directory):
             shutil.move(source_file_path, destination_file_path)
             break
 
-df = pd.read_csv(destination_file_path, delimiter=';')
+if destination_file_path:
+    df = pd.read_csv(destination_file_path, delimiter=';')
 
-data = df.to_dict(orient='records')
+    data = df.to_dict(orient='records')
 
-with open(f'/home/ricardo/code/statistic/src/credit_card/json/{month}.json', 'w', encoding='utf-8') as json_file:
-    json.dump(data, json_file, indent=4, ensure_ascii=False)
+    with open(f'/home/ricardo/code/statistic/src/credit_card/json/{month}.json', 'w', encoding='utf-8') as json_file:
+        json.dump(data, json_file, indent=4, ensure_ascii=False)
 
-# Format the numbers in the DataFrame to use Brazilian standards
-df['Valor (em R$)'] = df['Valor (em R$)'].apply(lambda x: f'{x:,.2f}'.replace('.', 'X').replace(',', '.').replace('X', ','))
+    # Format the numbers in the DataFrame to use Brazilian standards
+    def format_currency(value):
+        try:
+            return f'{float(value): , .2f}'.replace('.', 'X').replace(',', '.').replace('X', ',')
+        except ValueError:
+            return value
 
-# Exclude rows with "Inclusao de Pagamento" in the "Descrição" column
-df = df[df['Descrição'] != 'Inclusao de Pagamento    ']
+    df['Valor (em R$)'] = df['Valor (em R$)'].apply(format_currency)
 
-# Save the DataFrame to an Excel file with the desired format
-df.to_excel(f'/home/ricardo/code/statistic/src/credit_card/xlsx/{month}.xlsx', index=False, float_format="%.2f")
+    # Exclude rows with "Inclusao de Pagamento" in the "Descrição" column
+    df = df[df['Descrição'] != 'Inclusao de Pagamento']
 
-print(f'Invoice {month} has been moved and converted to .csv and .xlsx files')
+    # Save the DataFrame to an Excel file with the desired format
+    df.to_excel(f'/home/ricardo/code/statistic/src/credit_card/xlsx/{
+                month}.xlsx', index=False, float_format="%.2f")
+
+    print(
+        f'Invoice {month} has been moved and converted to .csv and .xlsx files')
+else:
+    print(f'No file with extension {
+          target_extension} found in {source_directory}')
